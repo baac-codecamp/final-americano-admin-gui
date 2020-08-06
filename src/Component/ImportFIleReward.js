de78a9abc78e41c6c1ca61151d79b404751ee01a
Kraisit.ch <kraisit.ch@baac.or.th>
2020-08-06 14:34:05 +0700

    [Edited]: Add Function ImportFileReward By ReadExcel

--- src/Component/ImportFIleReward.js
+++ src/Component/ImportFIleReward.js
@@ -1,63 +1,57 @@
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu, Breadcrumb } from 'antd'
 import HeaderAdmin from '../Component/HeaderAdmin'
-import logo from "../images/logo1.png";
-import {
-  DesktopOutlined,
-  PieChartOutlined,
-  FileOutlined,
-  TeamOutlined,
-  UserOutlined,
-} from '@ant-design/icons';
-import React from 'react';
-import { Link } from 'react-dom';
-import { GetName, Logout } from "../helpers";
-import SlideBar from "../Component/nav"
+import logo from '../images/logo1.png'
+import { DesktopOutlined, PieChartOutlined, FileOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons'
+import React from 'react'
+import { Link } from 'react-dom'
+import { GetName, Logout } from '../helpers'
+import SlideBar from '../Component/nav'
 import Papa from 'papaparse'
-const { Header, Content, Footer, Sider } = Layout;
-const { SubMenu } = Menu;
-let btn;
-
+import ImportFileNew from './ImportFileNew'
+const { Header, Content, Footer, Sider } = Layout
+const { SubMenu } = Menu
+let btn
 
 class FileReader extends React.Component {
   constructor() {
-    super();
+    super()
     this.state = {
-      csvfile: undefined
-    };
-    this.updateData = this.updateData.bind(this);
+      csvfile: undefined,
+    }
+    this.updateData = this.updateData.bind(this)
   }
 
-  handleChange = event => {
+  handleChange = (event) => {
     this.setState({
-      csvfile: event.target.files[0] || "0"
-    });
-  };
+      csvfile: event.target.files[0] || '0',
+    })
+  }
 
   importCSV = () => {
-    alert("TEST")
-    console.log(this.state);
-    const { csvfile } = this.state;
+    alert('TEST')
+    console.log(this.state)
+    const { csvfile } = this.state
     Papa.parse(csvfile, {
       complete: this.updateData,
-      header: true
-    });
-  };
+      header: true,
+    })
+  }
 
   updateData(result) {
-    var data = result.data;
-    console.log(data);
+    var data = result.data
+    console.log(data)
   }
 
   render() {
-    console.log(this.state.csvfile);
+    console.log(this.state.csvfile)
     return (
       <div>
         <h2>Import Reward File </h2>
         <input
           className="csv-input"
           type="file"
-          ref={input => {
-            this.filesInput = input;
+          ref={(input) => {
+            this.filesInput = input
           }}
           name="file"
           placeholder={null}
@@ -66,52 +60,51 @@ class FileReader extends React.Component {
         <p />
         <button onClick={this.importCSV}> Upload now</button>
       </div>
-    );
+    )
   }
 }
-
 
 class ImportFIleReward extends React.Component {
   state = {
     collapsed: false,
-  };
+  }
 
   componentDidMount() {
     this.setState({
       fullName: GetName(),
-    });
+    })
   }
 
   handleLogout = () => {
-    Logout();
+    Logout()
     this.setState({
       isLogout: true,
-    });
-  };
+    })
+  }
 
-  onCollapse = collapsed => {
-    console.log(collapsed);
-    this.setState({ collapsed });
-  };
+  onCollapse = (collapsed) => {
+    console.log(collapsed)
+    this.setState({ collapsed })
+  }
 
   render() {
     return (
-        <Layout style={{ minHeight: '100vh' }}>
-          <SlideBar></SlideBar>
-          <Layout className="site-layout">
-            <HeaderAdmin></HeaderAdmin>
-           
-            <Content style={{ margin: '0 16px' }}>
-              <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-                <FileReader></FileReader>
-              </div>
-            </Content>
-            <Footer style={{ textAlign: 'center' }}>BAAC©2020</Footer>
-          </Layout>
+      <Layout style={{ minHeight: '100vh' }}>
+        <SlideBar></SlideBar>
+        <Layout className="site-layout">
+          <HeaderAdmin></HeaderAdmin>
+
+          <Content style={{ margin: '0 16px' }}>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+              {/* <FileReader></FileReader> */}
+              <ImportFileNew></ImportFileNew>
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>BAAC©2020</Footer>
         </Layout>
-    );
+      </Layout>
+    )
   }
 }
 
-
-export default ImportFIleReward;
+export default ImportFIleReward
