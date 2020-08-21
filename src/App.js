8d5c4210bd8d0c3e6cec927b68694dda4b356b68
keerasak <benz4062@gmail.com>
2020-08-21 11:53:45 +0700

    edit logo import page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Login from './views/Login'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Admin from './views/Admin' 
+import { Redirect } from 'react-router';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Admin from './views/Admin'
 import 'antd/dist/antd.css'
 import './App.css'
 
@@ -9,16 +10,17 @@ class App extends React.Component {
   constructor(props) {
     super()
   }
-
   render() {
     return (
       <Router>
         <Switch>
-          <Route path="/login" exact={true} component={Login}/>
-          <Route path="/login/signup" exact={true} component={Login}/>
-          <Route path="/admin/reward" exact={true} component={Admin}/>
-          <Route path="/admin/customer" exact={true} component={Admin}/>
-          <Route path="/admin/news" exact={true} component={Admin}/>
+          {/* <Redirect from='/' to="/login" /> */}
+          <Route path="/login" exact={true} component={Login} />
+          <Route path="/login/signup" exact={true} component={Login} />
+          <Route path="/admin/reward" exact={true} component={Admin} />
+          <Route path="/admin/customer" exact={true} component={Admin} />
+          <Route path="/admin/news" exact={true} component={Admin} />
+          <Redirect from="*" to={"/login"} />
         </Switch>
       </Router>
     )
